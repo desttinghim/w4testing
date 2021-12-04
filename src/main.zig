ece17ad3d7b6a0339f75a979016a9e395679ccf2
Louis Pearson <louispearson@librem.one>
2021-12-04 12:13:50 -0700

    Add palettes
    Made a bunch I can select between easily

--- src/main.zig
+++ src/main.zig
@@ -1,4 +1,5 @@
 const w4 = @import("wasm4.zig");
+const palettes = @import("palettes.zig");
 
 const smiley = [8]u8{
     0b11000011,
@@ -11,15 +12,41 @@ const smiley = [8]u8{
     0b11000011,
 };
 
+const smiley2 = [8]u8{
+    0b11000011,
+    0b10000001,
+    0b00100000,
+    0b00100110,
+    0b00000000,
+    0b00100100,
+    0b10011001,
+    0b11000011,
+};
+
+export fn start() void {
+    w4.PALETTE.* = palettes.fuzzyfour;
+}
+
 export fn update() void {
     w4.DRAW_COLORS.* = 2;
     w4.text("Hello from Zig!", 10, 10);
 
+    w4.DRAW_COLORS.* = 1;
+    w4.rect(0, 152, 8, 8);
+    w4.DRAW_COLORS.* = 2;
+    w4.rect(8, 152, 8, 8);
+    w4.DRAW_COLORS.* = 3;
+    w4.rect(16, 152, 8, 8);
+    w4.DRAW_COLORS.* = 4;
+    w4.rect(24, 152, 8, 8);
+
     const gamepad = w4.GAMEPAD1.*;
     if (gamepad & w4.BUTTON_1 != 0) {
-        w4.DRAW_COLORS.* = 4;
+        w4.DRAW_COLORS.* = 3;
+        w4.blit(&smiley2, 76, 76, 8, 8, w4.BLIT_1BPP);
+    } else {
+        w4.blit(&smiley, 76, 76, 8, 8, w4.BLIT_1BPP);
     }
 
-    w4.blit(&smiley, 76, 76, 8, 8, w4.BLIT_1BPP);
-    w4.text("Press X to blink", 16, 90);
+    w4.text("Press X to wink", 16, 90);
 }
---
+++ src/palettes.zig
@@ -0,0 +1,65 @@
+/// Palette taken from
+pub const template = .{};
+
+/// Palette taken from
+pub const en4 = .{ 0xfbf7f3, 0xe5b083, 0x426e5d, 0x20283d };
+
+/// Palette taken from https://lospec.com/palette-list/red-poster
+pub const gbrp = .{ 0xe8d6c0, 0x92938d, 0xa1281c, 0x000000 };
+
+/// Palette taken from https://lospec.com/palette-list/ice-cream-gb
+pub const icecreamgb = .{ 0x7c3f58, 0xeb6b6f, 0xf9a875, 0xfff6d3 };
+
+/// Palette taken from https://lospec.com/palette-list/ayy4
+pub const ayy4 = .{ 0x00303b, 0xff7777, 0xffce96, 0xf1f2da };
+
+/// Palette taken from https://lospec.com/palette-list/kirokaze-gameboy
+pub const kirokazegb = .{ 0x332c50, 0x46878f, 0x94e344, 0xe2f3e4 };
+
+/// Palette taken from https://lospec.com/palette-list/rustic-gb
+pub const rusticgb = .{ 0x2c2137, 0x764462, 0xedb4a1, 0xa96868 };
+
+/// Palette taken from https://lospec.com/palette-list/mist-gb
+pub const mistgb = .{ 0x2d1b00, 0x1e505e, 0x5ab9a8, 0xc4f0c2 };
+
+/// Palette taken from https://lospec.com/palette-list/wish-gb
+pub const wishgb = .{ 0x622e4c, 0x7550e8, 0x608fcf, 0x8be5ff };
+
+/// Palette taken from https://lospec.com/palette-list/2bit-demichrome
+pub const demichrome = .{ 0x211e20, 0x555568, 0xa0a08b, 0xe9efec };
+
+/// Palette taken from https://lospec.com/palette-list/lava-gb
+pub const lavagb = .{ 0x051f39, 0x4a2480, 0xc53a9d, 0xff8e80 };
+
+/// Palette taken from https://lospec.com/palette-list/crimson
+pub const crimson = .{ 0xeff9d6, 0xba5044, 0x7a1c3b, 0x1b0326 };
+
+/// Palette taken from https://lospec.com/palette-list/hallowpumpkin
+pub const hallowpumpkin = .{ 0x300030, 0x602878, 0xf89020, 0xf8f088 };
+
+/// Palette taken from https://lospec.com/palette-list/spacehaze
+pub const spacehaze = .{ 0xf8e3c4, 0xcc3495, 0x6b1fb1, 0x0b0630 };
+
+/// Palette taken from https://lospec.com/palette-list/hollow
+pub const hollow2bit = .{ 0x0f0f1b, 0x565a75, 0xc6b7be, 0xfafbf6 };
+
+/// Palette taken from https://lospec.com/palette-list/soda-cap
+pub const sodacapp = .{ 0x2176cc, 0xff7d6e, 0xfca6ac, 0xe8e7cb };
+
+/// Palette taken from https://lospec.com/palette-list/nostalgia
+pub const nostalgia = .{ 0xd0d058, 0xa0a840, 0x708028, 0x405010 };
+
+/// Palette taken from https://lospec.com/palette-list/blk-aqu4
+pub const blkaqu4 = .{ 0x002b59, 0x0055f8c, 0x00b9be, 0x9ff4e5 };
+
+/// Palette taken from https://lospec.com/palette-list/moonlight-gb
+pub const moonlightgb = .{ 0x0f052d, 0x203671, 0x36868f, 0x5fc75d };
+
+/// Palette taken from https://lospec.com/palette-list/fuzzyfour
+pub const fuzzyfour = .{ 0x302387, 0xff3796, 0x00faac, 0xfffdaf };
+
+/// Palette taken from https://lospec.com/palette-list/cherrymelon
+pub const cherrymelon = .{ 0xfcdeea, 0xff4d6d, 0x265935, 0x012824 };
+
+// https://lospec.com/palette-list/coldfire-gb
+// https://lospec.com/palette-list/gold-gb
